407f101a8700497a4c3bebe3bcb3cb903358ad59
pq24aaa <pq24aaa@herts.ac.uk>
2026-02-23 15:02:44 +0000

    Started creating the Complex class, and added get.

--- mavenproject1/src/main/java/Complex.java
+++ mavenproject1/src/main/java/Complex.java
@@ -4,4 +4,22 @@ public class Complex {
     private double real;
     private double imaginary;
     
+    public Complex (double real, double imaginary){
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+    
+    public Complex (double real){
+        this.real = real;
+        this.imaginary = 0;
+    }
+    
+    public double getRealPart(){
+        return real;
+    }
+    
+    public double getimaginaryPart(){
+        return imaginary;
+    }
+    
     
